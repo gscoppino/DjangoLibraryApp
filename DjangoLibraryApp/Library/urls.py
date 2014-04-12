8e1e6df74988a67bf9758c205a0fbf045fb30ddc
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2014-04-12 18:33:44 -0400

    all frontend work

---
+++ DjangoLibraryApp/Library/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from Library import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^library/', views.IndexView.as_view(), name='detail_system'),
+    url(r'^library/(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^book/(?P<book_id>\d+)/checkout/$', views.checkout, name='checkout')
+)
---
+++ DjangoLibraryApp/Library/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+
+from Library.models import * 
+# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'Library/index.html'
+    context_object_name = 'library_list'
+
+    def get_queryset(self):
+        return Library.objects.order_by('name')
+
+class SystemBookView(generic.ListView):
+    template_name = 'Library/system_books.html'
+    context_object_name = 'book'
+    
+    def get_queryset(self):
+        return Book.objects.order_by('name')
+
+class DetailView(generic.DetailView):
+    model = Library 
+    template_name = 'Library/student_grade.html' #temp
+
+def checkout(request, book_id):
+    return HttpResponse("Now checking out Book %s" % book_id)  
